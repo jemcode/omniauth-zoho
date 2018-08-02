8cb9a936c769cad6e4ee5af98edc545a3b5fa924
Mike Machado <mike@machadolab.com>
2018-08-02 11:56:59 -0700

    Clean up expires msec vs sec handling

--- lib/omniauth/strategies/zoho.rb
+++ lib/omniauth/strategies/zoho.rb
@@ -26,6 +26,14 @@ module OmniAuth
         }
       end
 
+      credentials do
+        hash = {"token" => access_token.token}
+        hash.merge!("refresh_token" => access_token.refresh_token) if access_token.expires? && access_token.refresh_token
+        hash.merge!("expires_at" => Time.now.to_i + 3600)
+        hash.merge!("expires" => true)
+        hash
+      end
+
       def raw_info
         @raw_info ||= access_token.get('https://www.zohoapis.com/crm/v2/org').parsed['org'].first
       end
