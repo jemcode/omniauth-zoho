45864efedba94d49fe30f84e020f9ec4314abf62
Khamsouk Souvanlasy <khamsouk@souvanlasy.com>
2018-12-20 15:36:49 +1100

    Remove unnecessary code

--- lib/omniauth/strategies/zoho.rb
+++ lib/omniauth/strategies/zoho.rb
@@ -12,31 +12,10 @@ module OmniAuth
 
       option provider_ignores_state: true
 
-      uid{ raw_info['id'] }
+      private
 
-      info do
-        {
-            :email => raw_info['primary_email'],
-        }
-      end
-
-      extra do
-        {
-            'raw_info' => raw_info
-        }
-      end
-
-      credentials do
-        hash = {"token" => access_token.token}
-        hash.merge!("refresh_token" => access_token.refresh_token) if access_token.expires? && access_token.refresh_token
-        hash.merge!("expires_at" => Time.now.to_i + 3600)
-        hash.merge!("expires" => true)
-        hash
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get('https://www.zohoapis.com/crm/v2/org').parsed['org'].first
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
     end
   end
-end
